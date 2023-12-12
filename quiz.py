0fa729812d5168320dc92de1d434f97b2eb7cc89
Alexander Mann-Wahrenberg <alexander.mannwahrenberg@gmail.com>
2023-12-12 19:45:15 +0000

    Beginn der Highscores

--- quiz.py
+++ quiz.py
@@ -1,20 +1,21 @@
-################################################################################
+###########################################################################################################
 # (a) Quiz im Stil von „Wer wird Millionär“
-################################################################################
-#                         Author: Juliane Wahrenberg
+###########################################################################################################
+#                         Autor: Juliane Wahrenberg
 #
 # (b) Dem Benutzer werden der Reihe nach Fragen mit vier möglichen Antworten
 #     präsentiert.
 # (c) Aus diesen kann er wählen, indem er die entsprechende Taste (a, b, c oder
 #     d) benutzt.
+# (d) Der Benutzer kann einmal in der Runde ein 50/50 Joker anwenden undn das Spiel vorzeitig beenden
+# (e) Der Benutzer kann mit richtigen Antworten Punkte erreichen und das Spiel vorzeitig beenden
 #
-#
-################################################################################
+###########################################################################################################
 
 # *** Anfang des Programms ***
 
 # Begrüßung (a)
-print("\nLang ist es her und jetzt ist es endlich wieder soweit:\nHerzlich willkommen zu einer neuen Ausgabe von Wer wird Millionär!\n")
+print("\nLang ist es her und jetzt ist es endlich wieder soweit:\nHerzlich willkommen zu einer neuen Ausgabe von Wer wird Millionär!\nDas heutige Thema lautet: \"Quer durch Afrika\"\nEs geht direkt los!\n")
 
 # Liste an Fragen (b)
 fragen = [
@@ -22,11 +23,11 @@ fragen = [
    ["Wie viele Giraffen leben in Afrika?", "2.500", "68.000", "15.000", "79.000", "B", "50", [1,3]],
    ["Wie viele Tage hat ein SchaltJahr in Afrika?", "364", "510", "365", "366", "D", "100", [1,3]],
    ["Auf welchem Komntinent liegt Timbuktu?", "Asien", "Australien", "Deutschland", "Afrika", "D", "200", [1,3]],
-   ["d", "50", "60", "70", "80", "D", "300", [1,3]],
+   ["In welchem afrikanischen Land kann man die meißten Pyramiden bewundern?", "Elfenbeinküste", "Ägypten", "Marokko", "Sudan", "D", "300", [1,3]],
    ["Welche Sprache wird in Südafrika hauptsächlich gesprochen?", "Französisch", "Spanisch", "Englisch","Östereichisch", "C", "500", [1,3]],
-   ["Die Ägyptischen Pyramiden sind nicht nur sehr bekannt, sondern auch sehr groß.\n Wie viele Elefanten ist die größte Pyramide, die cheops Pyramide, hoch?", "50", "105", "43", "19", "C", "1000", [1,4]],
+   ["Die Ägyptischen Pyramiden sind nicht nur sehr bekannt, sondern auch sehr groß.\nWie viele Elefanten ist die größte Pyramide, die cheops Pyramide, hoch?", "50", "105", "43", "19", "C", "1000", [1,4]],
    ["Welche Tierart ist untypisch für die afrikanische Savanne?", "Nilpferde", "Nashörner", "Pinguine", "Strauße", "C", "2000", [1,2]],
-   ["Wie viele Läner liegen auf dem Kontinent Afrika?", "55", "88", "5", "33", "A", "4000", [2,3]],
+   ["Wie viele Läner liegen auf dem Kontinent Afrika?", "54", "88", "5", "33", "A", "4000", [2,3]],
    ["Welcher Ozean liegt im Westen von Afrika", "Indische Ozean", "Atlanitsche Ozean", "Mittelmeer", "pazifische Ozean", "B", "8000", [1,3]],
    ["Wie heißt der längste Fluss in Afrika?", "Niger", "Kongo", "Rhein", "Nil", "D", "16000", [1,3]],
    ["Wie viel größer ist Europa als Afrika?", "doppelt so groß", "fünf mal so groß", "sieben mal so groß", "dreimal so groß", "D", "32000", [1,3]],
@@ -47,6 +48,7 @@ while antwort.upper() == "JA":
    while i < 15:
       frage = fragen[i]
       # Präsentation der Frage (b)
+        # Joker angewendet (d)
       print("Mit der folgenden Frage erreichst Du", frage[6], "Punkte:")
       print(frage[0])
       if joker_ist_aktiv == True:
@@ -69,7 +71,7 @@ while antwort.upper() == "JA":
             print("D:", frage[4], " <- IST FALSCH.")
          else:
              print("D:", frage[4])
-
+         # Joker nicht angewendet (d)
          joker_ist_aktiv = False
       else:
          print("A:", frage[1])
@@ -77,7 +79,7 @@ while antwort.upper() == "JA":
          print("C:", frage[3])
          print("D:", frage[4])
 
-# Fragen nach Antwort
+# Fragen nach Antwort (c,d)
       if anzahl_joker > 0:
          gueltige_antworten = ["A", "B", "C", "D", "JOKER", "ENDE"]
       else:
@@ -87,16 +89,16 @@ while antwort.upper() == "JA":
          if i == 0:
             print("Was ist deine Antwort?")
          else:
-            print("Was denkst du dieses Mal? Oder nimm die bis jetzt erreichten Punkte mit nach hause und beende das Spiel mit \"Ende\".")
+            print("Was denkst du dieses Mal?\nDu kannst auch deine bis jetzt erreichten Punkte mit nach hause und beende das Spiel mit \"Ende\" beenden.")
          if anzahl_joker > 0:
-            print("Du kannst aber mit \"Joker\" auch Deinen 50:50 Joker einlösen.")
+            print("Du kannst auch mit \"Joker\" auch Deinen 50:50 Joker einlösen.\nAber denk dran: Du hast nur einen in der gesamten Runde.\n")
          antwort = input()
          if not antwort.upper() in gueltige_antworten:
             print("Das habe ich nicht verstanden. Bitte gib' a, b, c, d Ende ein!")
             if anzahl_joker > 0:
                print("Oder Joker.")
 
-# Reaktion auf Antwort
+# Reaktion auf Antwort (e)
       if antwort.upper() == frage[5]:
          print("Perfekt! Du hast jetzt", (frage[6]), "Punkte.\n")
       elif antwort.upper() == "JOKER":
@@ -119,4 +121,4 @@ while antwort.upper() == "JA":
    if antwort.upper() == "JA":
       print("Auf gehts zur nächsten Runde!")
 
-print("Tschüss")
+print("Na dann auf Wiedersehen")
